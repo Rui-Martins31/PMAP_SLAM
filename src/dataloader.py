cecbf67609d4b95b07dbc0004c5d007ad6262545
carreira2002 <up202007783@up.pt>
2025-12-11 11:01:51 +0000

    Aula

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import src.__config as GLOBALS
 
+
+# Test
+def load_data_using_pd(data_path, debug: bool = False):
+    
+    
+    pass
 
 # Load data function
 def load_data(data_path, debug:bool=False):
@@ -34,7 +41,27 @@ def convert_data_to_points(distance, debug:bool=False):
 
     return x, y
 
+def plot_points(x: np.ndarray, y: np.ndarray, save_plot: bool = False):
+    # Plot
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y, color='blue', alpha=0.5, label='Data Points')
+
+    plt.title('LiDAR Points')
+    plt.xlabel('X coordinates')
+    plt.ylabel('Y coordinates')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    # Save
+    if save_plot:
+        plt.savefig('scatter_plot.png')
+
 # Main
 if __name__ == "__main__":
     travelled_distance, measured_variation, lidar_measurements=load_data(data_path=GLOBALS.PATH_DATASET, debug=GLOBALS.DEBUG)
-    x, y=convert_data_to_points(lidar_measurements, debug=GLOBALS.DEBUG)
+    print(lidar_measurements.shape)
+    for i in range(len(lidar_measurements)):
+        x, y=convert_data_to_points(lidar_measurements[70], debug=GLOBALS.DEBUG)
+        plot_points(x, y)
+        break
